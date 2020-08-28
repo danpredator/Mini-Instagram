5c068c039a6ec28e686d864fecba96bb76f28ebf
danpredator <43988717+danpredator@users.noreply.github.com>
2020-08-28 21:25:06 +0530

    changed frames to post-size buttons

--- profile.py
+++ profile.py
@@ -209,57 +209,16 @@ class Newsfeed(tk.Frame):
         self.scrl_can.create_window((0,0),window=self.Frame_1,anchor='nw')
         self.Frame_1.bind("<Configure>",lambda event: self.scrl_can.configure(scrollregion=self.scrl_can.bbox("all")))
 
-        self.Frame_p = tk.Frame(self.scrl_can)# doesnt wrk wit frame
-        self.Frame_p.place(relx=0.0, rely=0.0, relheight=1.0, relwidth=1.0)
-        self.Frame_p.configure(relief="groove")
-        self.Frame_p.configure(background="#d9d9d9")
-                
-
-        self.Label2 = tk.Label(self.Frame_p)
-        self.Label2.place(relx=0.0, rely=0.0, height=51, width=450)
-        self.Label2.configure(anchor='w')
-        self.Label2.configure(background="#d9d9d9")
-        self.Label2.configure(disabledforeground="#a3a3a3")
-        self.Label2.configure(foreground="#000000")
-        self.Label2.configure(state='active')
-        self.Label2.configure(text='username_f')
-
-        self.Canv_postPic = tk.Canvas(self.Frame_p)
-        self.Canv_postPic.place(relx=0.0, rely=0.118, relheight=0.689
-                , relwidth=1.0)
-        self.Canv_postPic.configure(background="#d9d9d9")
-        #self.Canv_postPic.configure(borderwidth="2")
-        self.Canv_postPic.configure(insertbackground="black")
-        self.Canv_postPic.configure(relief="ridge")
-        self.Canv_postPic.configure(selectbackground="#c4c4c4")
-        self.Canv_postPic.configure(selectforeground="black")
-
-        self.Button6 = tk.Button(self.Frame_p)
-        self.Button6.place(relx=0.0, rely=0.8, height=51, width=57)
-        self.Button6.configure(activebackground="#ececec")
-        self.Button6.configure(activeforeground="#000000")
-        self.Button6.configure(background="#d9d9d9")
-        self.Button6.configure(disabledforeground="#a3a3a3")
-        self.Button6.configure(foreground="#000000")
-        self.Button6.configure(highlightbackground="#d9d9d9")
-        self.Button6.configure(highlightcolor="black")
-        self.Button6.configure(pady="0")
-        self.Button6.configure(text='Like')
-
-        self.Label3 = tk.Label(self.Frame_p)
-        self.Label3.place(relx=0.111, rely=0.8, height=51, width=404)
-        self.Label3.configure(background="#e8003a")
-        self.Label3.configure(disabledforeground="#a3a3a3")
-        self.Label3.configure(foreground="#000000")
-        self.Label3.configure(text='cmt_shareimg')
-
-        self.Label4 = tk.Label(self.Frame_p)
-        self.Label4.place(relx=0.0, rely=0.925, height=30, width=134)
-        self.Label4.configure(background="#15ff33")
-        self.Label4.configure(disabledforeground="#a3a3a3")
-        self.Label4.configure(font=font9)
-        self.Label4.configure(foreground="#000000")
-        self.Label4.configure(text='N_Likes')
+        global _img         
+        _img = ImageTk.PhotoImage(Image.open('storyimg.png').resize((400,300),Image.ANTIALIAS))
+        
+        for i in range(0,5):
+            #f = tk.Frame(self.Frame_1, height = 100, width = 100,bg="yellow")
+            #f.pack_propagate(0) 
+            #f.place(x = 0, y = i+100)
+            tk.Button(self.Frame_1,text=None,font=font9,compound="right",image=_img,background="#eeeeee",borderwidth="0").grid(row=i,column=0,padx=10, pady=10)
+            #tk.Label(self.Frame_1,text="Images",font=font9,image=_img,compound="right").grid(row=i,column=0)
+            #tk.Label(self.Frame_1,image=img,bg="blue").grid(row=i+1,column=0)
         
     
         
